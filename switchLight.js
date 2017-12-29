fef4f123f5598893ae1da91902238d5765851490
mmarchoux <mathilde.marchoux@etu.emse.fr>
2017-12-29 02:28:15 +0100

    add -- add switch all lights with light ON or light OFF

--- switchLight.js
+++ switchLight.js
@@ -33,29 +33,40 @@ const v2 = new Vue({
 
 
     methods: {
-        switchAllLight: function(state) {
+        switchAllLights: function(state) {
 
-            if(this.state=='ON'){
-                listRooms = roomsOn;
+            //select the list corresponding to the state
+            if(state === 'ON'){
+                listRooms = this.roomsOn;
             }
             else{
-                listRooms = roomsOff;
+                listRooms = this.roomsOff;
             }
 
+            //make the request
             for(var i = 0; i<listRooms.length; i++){
                 var room = listRooms[i];
-                switchLight(room);
+                axios.put('https://enigmatic-sands-54357.herokuapp.com/api/rooms/'+room.id+'/switchlight',{})
+                .then(response => {
+                    this.rooms = response.data
+                })
             }
+
+            //reload the page
+            window.location.reload(false);
         
         },
 
 
         switchLight: function(room) {
+
+            //make the request
             axios.put('https://enigmatic-sands-54357.herokuapp.com/api/rooms/'+room+'/switchlight',{})
             .then(response => {
                 this.rooms = response.data
             })
 
+            //reload the page to update, I haven't found how to only update the dropdown menus
             window.location.reload(false);
         }
     }
