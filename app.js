a755e5ff1ef2c118ed81a28a215fd3504939787b
mmarchoux <mathilde.marchoux@etu.emse.fr>
2018-01-11 10:23:37 +0100

    last fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const v = new Vue({
+const x = new Vue({
     el: "#app",
     data: {
         rooms: [],
     },
     
     mounted() {
-        axios.get("https://enigmatic-sands-54357.herokuapp.com/api/rooms")
+        axios.get("https://infinite-savannah-53354.herokuapp.com/api/rooms/")
             .then(response => {
                 this.rooms = response.data
                 console.log( this.rooms)
@@ -13,20 +13,19 @@ const v = new Vue({
     },
     methods: {
        switchLight: function(room) {
-        axios.put('https://enigmatic-sands-54357.herokuapp.com/api/rooms/'+room.id+'/switchlight')
+        axios.put('https://infinite-savannah-53354.herokuapp.com/api/rooms/'+room.id+'/switchlight')
             .then(response => {
-            	this.rooms = response.data
+                this.rooms = response.data
             })
-        },
-        switchRinger: function(room) {
-        axios.put('https://enigmatic-sands-54357.herokuapp.com/api/rooms/'+room.id+'/switchringer')
+    },
+    switchRinger: function(room) {
+        axios.put('https://infinite-savannah-53354.herokuapp.com/api/rooms/'+room.id+'/switchringer')
             .then(response => {
-            	this.rooms = response.data
+                this.rooms = response.data
             })
-        }
     }
 
+
+}
+
 })
-
-
-
